cff9fcbfa79abc75680bac46963c574239e6af51
simonhiuhu-Leo <simon.hiuhu@student.moringaschool.com>
2026-06-14 11:15:16 +0300

    Fix CashRegister constructor and methods; add discount and void transaction behavior

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  def init(self,discount=0,total=0):
-    self.discount = discount
-    self.total = total
-    self.items = []
-    self.previous_transactions = []
+    def __init__(self, discount=0, total=0):
+        self.discount = discount
+        self.total = total
+        self.items = []
+        self.previous_transactions = []
 
-  @property
-  def discount(self):
-      return self._discount
-  @discount.setter
-  def discount(self,value):
-      if isinstance(value,int) and 0 <= value <= 100:
-        self._discount = value
-      else: 
-        print("Not valid discount")
+    @property
+    def discount(self):
+        return self._discount
 
-  def add_item(self,item,price,quantity = 1):
-      self.total += pricequantity
-      self.items += [item]quantity
-      self.previous_transactions.append((item,price,quantity))
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
 
-  def apply_discount(self):
-      if self._discount > 0:
-        self.total -= self.total*(self._discount/100)
-        print(f"After the discount, the total comes to ${int(self.total)}.")
-      else:
-        print("There is no discount to apply.")
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([item] * quantity)
+        self.previous_transactions.append((item, price, quantity))
 
-  def void_last_transaction(self):
-      ln = len(self.previous_transactions)
-      if ln != 0 :
-        self.total -= (self.previous_transactions[ln-1][1]*self.previous_transactions[ln-1][2])
-        self.previous_transactions.pop()
-      else:
-        print("There is no transaction to void.")
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total -= self.total * (self.discount / 100)
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.previous_transactions:
+            item, price, quantity = self.previous_transactions.pop()
+            self.total -= price * quantity
+            self.items = self.items[:-quantity]
+        else:
+            print("There is no transaction to void.")
